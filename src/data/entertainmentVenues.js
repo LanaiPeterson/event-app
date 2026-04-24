b3d859bfe2375192c3d7b6896f07fc0dc6126885
Lanai Peterson <lanaipeterson@gmail.com>
2026-04-24 13:50:04 -0400

    Create entertainmentVenues.js with a list of supported entertainment venues

---
+++ src/data/entertainmentVenues.js
@@ -0,0 +1,36 @@
+// List of supported entertainment venues and places
+
+const entertainmentVenues = [
+    {
+        name: 'Central Park',
+        type: 'Park',
+        location: 'New York City, NY'
+    },
+    {
+        name: 'Hollywood Bowl',
+        type: 'Theater',
+        location: 'Los Angeles, CA'
+    },
+    {
+        name: 'Walt Disney Concert Hall',
+        type: 'Concert Hall',
+        location: 'Los Angeles, CA'
+    },
+    {
+        name: 'Madison Square Garden',
+        type: 'Arena',
+        location: 'New York City, NY'
+    },
+    {
+        name: 'The O2',
+        type: 'Arena',
+        location: 'London, UK'
+    },
+    {
+        name: 'Sydney Opera House',
+        type: 'Opera House',
+        location: 'Sydney, Australia'
+    }
+];
+
+export default entertainmentVenues;
